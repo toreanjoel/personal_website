82069e7929bcef5259b68f0d1cc31f530474b98d
Torean Joel <toreanjoel@gmail.com>
2019-06-16 15:41:19 +0200

    remove quotes so we can send the string through to the mail for the message

--- js/helpers.js
+++ js/helpers.js
@@ -63,17 +63,22 @@ let EMAIL = {
         terminalCallbackMsg(EMAIL_MSG.validationErr.email, true);
     },
     isEmailMsgValid: function(emailMsg) {
-      return emailMsg !== '' ||
+      return emailMsg !== '' &&
+        ((emailMsg.charAt(0) === "'" || emailMsg.charAt(0) === "\"") &&
+        (emailMsg.charAt(emailMsg.length-1) === "'" || emailMsg.charAt(emailMsg.length-1) === "\"")) ||
         terminalCallbackMsg(EMAIL_MSG.validationErr.msg, true);
     }
   },
   send: function(details) {
     if(details) {      
       const email = details[0];
-      const msg = details.splice(1,).join(' ').replace(/["']/g, "");
-
+      // const msg = details.splice(1,).join(' ').replace(/["']/g, "");
+      const msg = details.splice(1,).join(' ');
+      // here we replace the first and last character of the message - the quotes
+      
       if(EMAIL.validation.isEmailvalid(email) &&
-        EMAIL.validation.isEmailMsgValid(msg)) {
+      EMAIL.validation.isEmailMsgValid(msg)) {
+        const removedQuotes = msg.slice(1,-1);
         // sending mail message...
         terminalCallbackMsg(EMAIL_MSG.sending, false);
         // update the SMTP and hosting
@@ -81,8 +86,8 @@ let EMAIL = {
           SecureToken : "5d01aa18-e880-480b-af79-4f77ee71f7d4",
           To : "toreanjoel@gmail.com",
           From : email,
-          Subject : "TrojanMorse, You got mail through your website!",
-          Body : msg
+          Subject : "TrojanMorse, You got mail!",
+          Body : removedQuotes
         }).then(
           resp => {
             if(resp === "OK") {
